8c8236b2bc9c5972dc3d49ea0d0e6411b7a4ae91
Erick Vieira <erick-vieira-s@outlook.com>
2018-12-18 13:39:47 -0200

    iniciando o projeto

---
+++ src/constants/project-constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'http://localhost:3000'
+export const SITE_URL = 'grupow.net'
+export const VERSION = '0.0.1'
+export const API_MAPS = 'mapsApiKeyContent';
---
+++ src/pages/access/acess.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { LoginPage } from './login/login';
+import { IonicModule } from 'ionic-angular';
+
+@NgModule({
+  declarations: [
+    LoginPage
+  ],
+  imports: [
+    IonicModule
+  ],
+  entryComponents: [
+    LoginPage
+  ],
+  providers: [
+  ]
+})
+export class AccessModule {}
